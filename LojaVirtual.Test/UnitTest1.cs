7db7fc3e0aa79e4c0fd701445b909be295000346
Henrique <Henrique@gmail.com>
2017-03-09 14:43:15 -0300

    Vitrine Aula 7

--- LojaVirtual.Test/UnitTest1.cs
+++ LojaVirtual.Test/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace LojaVirtual.Test
 {
@@ -7,8 +8,27 @@ namespace LojaVirtual.Test
     public class UnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Take()
         {
+            int[] numeros = { 5, 6, 7, 8, 9, 10,1,2,3,4,5 };
+
+            var resultado = from num in numeros.Take(5) select num;
+
+            int[] Teste = { 5, 6, 7, 8, 9 };
+
+            CollectionAssert.AreEqual(resultado.ToArray(), Teste);
+        }
+
+        public void Skype()
+        {
+            int[] numeros = { 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5 };
+
+            var resultado = from num in numeros.Take(5).Skip(5) select num;
+
+            int[] Teste = { 10, 1, 2, 3, 4 };
+
+            CollectionAssert.AreEqual(resultado.ToArray(), Teste);
+
 
         }
     }
---
+++ LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,25 @@
+﻿using LojaVirtual.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        private Produto Produtos;
+        public int ProdutosPorPagina = 3;
+            
+
+        public ActionResult ListaProdutos(int pagina = 1)
+        {
+            Produtos = new Produto();
+            var Itens = Produtos.Produtos.OrderBy(a=>a.Nome).Skip((pagina - 1) * ProdutosPorPagina).Take(ProdutosPorPagina) ;
+
+
+            return View(Itens);
+        }
+    }
+}
